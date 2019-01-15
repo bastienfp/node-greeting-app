ed27f0bc255a02c09d59d454cf6c8f77b5940ced
bastienfp <bastien.fp@gmail.com>
2019-01-15 10:14:16 -0500

    Completed checkpoint Basic Node App

---
+++ closing.js
@@ -0,0 +1,15 @@
+exports.closingEnglish = function() {
+  console.log("Goodbye");
+}
+
+exports.closingJapanese = function() {
+  console.log("Sayōnara");
+}
+
+exports.closingSpanish = function() {
+  console.log("Adiós");
+}
+
+exports.closingIcelandic = function() {
+  console.log("Bless");
+}
---
+++ main.js
@@ -0,0 +1,22 @@
+const greetings = require("./greeting.js");
+const closings = require("./closing.js");
+
+// "Hello"
+greetings.greetingEnglish();
+// "Goodbye"
+closings.closingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+// "Sayōnara"
+closings.closingJapanese();
+
+// "Hola"
+greetings.greetingSpanish();
+// "Adiós"
+closings.closingSpanish();
+
+// "Halló"
+greetings.greetingIcelandic();
+// "Bless"
+closings.closingIcelandic();
